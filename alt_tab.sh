6e5970e0c7e0f288798e9551254ea66ede3c3a6c
David Roussov <david.roussov@gmail.com>
2025-02-06 00:41:10 +0000

    update alt_tab

--- alt_tab.sh
+++ alt_tab.sh
@@ -2,27 +2,27 @@
 
 DEFAULT_COLOR="#f5f3f4"
 
-session_name=$(tmux display-message -p '#S')
+session_name=$(/nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux display-message -p '#S')
 
 case "$session_name" in
     "r")
-        tmux switch-client -t p
+        /nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux switch-client -t p
         foreground_color="#CBA6E3" # purple
         ;;
     "g")
-        tmux switch-client -t r
+        /nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux switch-client -t r
         foreground_color="#FF9999" # red
         ;;
     "b")
-        tmux switch-client -t g
+        /nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux switch-client -t g
         foreground_color="#77DD77" # green
         ;;
     "y")
-        tmux switch-client -t b
+        /nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux switch-client -t b
         foreground_color="#99CCFF" # blue
         ;;
     "p")
-        tmux switch-client -t y
+        /nix/store/3ac7m1dbzd54c6ygv0g880zbmw7dhhg3-tmux-3.5a/bin/tmux switch-client -t y
         foreground_color="#FFEB99" # yellow
         ;;
     *)
